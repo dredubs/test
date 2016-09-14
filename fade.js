c67063b3afb1edfac2a35c51f6f3b4345afeb1fa
dredubs <somnambulist@gmail.com>
2016-09-14 10:51:46 -0400

    Add files via upload

---
+++ fade.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+$('.toBeFaddedIn').hide();
+
+
+$('#righthand').delay(1000)
+.fadeIn(2000);
+
+$('#lefthand').delay(2000)
+.fadeIn(3000);
+
+
+
+
+
+// WILL FADE EVERYTHING OVER 2 SECONDS
+// $('.toBeFaddedIn').fadeIn(2000);
+	
+
+
+	// 	.delay(3000)
+	// 	.fadeIn(2000);
+
+
+
+});
