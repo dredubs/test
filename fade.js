6f6f57501c149a5a4b142f3f977f3921c66288e7
dredubs <somnambulist@gmail.com>
2016-09-15 12:47:29 -0500

    Update fade.js

--- fade.js
+++ fade.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
 $('.toBeFaddedIn').hide();
 
 
-$('#righthand').delay(1000)
-.fadeIn(2000);
-
-$('#lefthand').delay(2000)
+$('#righthand').delay(2000)
 .fadeIn(3000);
 
-$('#swearing').delay(3000)
-.fadeIn(4000);
+$('#lefthand').delay(4000)
+.fadeIn(5000);
+
+$('#swearing').delay(6000)
+.fadeIn(7000);
 
 
 
